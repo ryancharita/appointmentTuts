4efd12d3a5715697bc3c46d1ca00f24805732e70
Ryan Joshua Charita <rjcharita23@gmail.com>
2023-06-30 09:01:53 +0800

    create appointment using mobx

--- src/components/appointments/appointmentsTable.jsx
+++ src/components/appointments/appointmentsTable.jsx
@@ -1,56 +1,50 @@
-import { useContext } from "react";
+import { useContext } from 'react';
 
-import { App, Button, Popconfirm, Space, Table } from "antd";
-import { DeleteTwoTone, EditTwoTone, ExpandOutlined } from "@ant-design/icons";
-import { observer } from "mobx-react";
-import moment from "moment";
-import { useNavigate } from "react-router-dom";
+import { App, Button, Popconfirm, Space, Table } from 'antd';
+import { DeleteTwoTone, EditTwoTone, ExpandOutlined } from '@ant-design/icons';
+import { observer } from 'mobx-react';
+import moment from 'moment';
+import { useNavigate } from 'react-router-dom';
 
-import { MyContext } from "../../context/context.jsx";
+import { MyContext } from '../../context/context.jsx';
 
 const AppointmentsTable = observer(() => {
-  const { appointmentList, setAppointment, setEditRecord, setModalDisplay } =
-    useContext(MyContext);
+  const { appointmentList, setAppointment, setEditRecord, setModalDisplay } = useContext(MyContext);
 
   const { notification } = App.useApp();
   const navigate = useNavigate();
 
   const columns = [
     {
-      title: "Name",
-      dataIndex: "name",
-      key: "name",
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
     },
     {
-      title: "Contact Number",
-      dataIndex: "contact_number",
-      key: "contact_number",
+      title: 'Contact Number',
+      dataIndex: 'contact_number',
+      key: 'contact_number',
     },
     {
-      title: "Purpose",
-      dataIndex: "purpose",
-      key: "purpose",
+      title: 'Purpose',
+      dataIndex: 'purpose',
+      key: 'purpose',
     },
     {
-      title: "Purpose",
-      dataIndex: "purpose",
-      key: "purpose",
+      title: 'Appointment Date',
+      dataIndex: 'appointment_date',
+      key: 'appointment_date',
+      render: (text) => <a>{moment(text).format('YYYY-MM-DD')}</a>,
     },
     {
-      title: "Appointment Date",
-      dataIndex: "appointment_date",
-      key: "appointment_date",
-      render: (text) => <a>{moment(text).format("YYYY-MM-DD")}</a>,
+      title: 'Appointment Time',
+      dataIndex: 'appointment_time',
+      key: 'appointment_time',
+      render: (text) => <a>{moment(text).format('hh:mm:ss')}</a>,
     },
     {
-      title: "Appointment Time",
-      dataIndex: "appointment_time",
-      key: "appointment_time",
-      render: (text) => <a>{moment(text).format("hh:mm:ss")}</a>,
-    },
-    {
-      title: "Actions",
-      key: "actions",
+      title: 'Actions',
+      key: 'actions',
       render: (_, record) => (
         <Space size="middle">
           {/* Edit button */}
@@ -105,14 +99,14 @@ const AppointmentsTable = observer(() => {
       setAppointment([...appointmentList]);
       notification.success({
         message: `Delete Success`,
-        description: "Appointment Deleted",
-        placement: "topRight",
+        description: 'Appointment Deleted',
+        placement: 'topRight',
       });
     } catch (error) {
       notification.error({
         message: `Something went wrong!`,
         description: error.message,
-        placement: "topRight",
+        placement: 'topRight',
       });
     }
   };
